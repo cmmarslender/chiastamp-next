94078b640ce02dce8892210d5644c8d4ef064fd1
Chris Marslender <chrismarslender@gmail.com>
2025-09-29 22:34:50 -0500

    Make the filename sane

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home(): React.ReactNode {
             const url = URL.createObjectURL(blob);
             const a = document.createElement("a");
             a.href = url;
-            a.download = "proof.json";
+            a.download = `${fileName}.proof.json`;
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
